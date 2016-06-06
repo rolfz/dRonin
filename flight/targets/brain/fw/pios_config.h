e9ceaeb5bc78b56ac8a21cebf1b5460129a007d7
rolfz <rolf.ziegler@z-control.ch>
2016-06-06 21:07:18 +0200

    MPXV7002 enabled

--- flight/targets/brain/fw/pios_config.h
+++ flight/targets/brain/fw/pios_config.h
@@ -66,6 +66,8 @@
 #define PIOS_INCLUDE_HMC5983_I2C
 #define PIOS_INCLUDE_ETASV3
 #define PIOS_I2C_ETASV3_ADAPTER pios_i2c_flexi_id
+#define PIOS_INCLUDE_MPXV5004
+#define PIOS_INCLUDE_MPXV7002
 
 /* Com systems to include */
 #define PIOS_INCLUDE_COM
